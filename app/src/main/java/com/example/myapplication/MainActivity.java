3f8c4eaacad134664c9fd29066fd0b50272a8144
Aleksandr Beskakotov <beskakotov.aleksandr@gmail.com>
2019-11-22 10:14:17 +0200

    add "Check" button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,9 +6,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class MainActivity extends LoggingActivity {
 
     private static final String KEY_CURRENT_INDEX = "key_current_index";
+    private static final String NO_ANSWER = "no_answer";
+    private static final String CORRECT_ANSWER = "correct_answer";
+    private static final String INCORRECT_ANSWER = "incorrect_answer";
 
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_australia, true),
@@ -20,11 +25,21 @@ public class MainActivity extends LoggingActivity {
     };
 
     private int mCurrentIndex = 0;
+    private int numberOfAnswers = 0;
+    private int numberOfCorrectAnswers = 0;
+    private int numberOfQuestions = mQuestionBank.length;
+
+    private String[] answers = new String[mQuestionBank.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int pos = 0; pos < mQuestionBank.length; pos++ ){
+            answers[pos] = NO_ANSWER;
+        }
+
 
         if (savedInstanceState != null) {
             mCurrentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
@@ -60,7 +75,26 @@ public class MainActivity extends LoggingActivity {
                 questionString.setText(currentQuestion.getQuestionResId());
             }
         });
+
+        Button checkButton = findViewById(R.id.check_button);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toastMessage = String.format("Answered %d/%d questions\n " +
+                                      "Correct answers: %d", numberOfAnswers, numberOfQuestions, numberOfCorrectAnswers);
+
+                Toast.makeText(
+                        getApplicationContext(),
+                        toastMessage,
+                        Toast.LENGTH_LONG
+                ).show();
+
+            }
+        });
+
+
     }
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -79,5 +113,30 @@ public class MainActivity extends LoggingActivity {
                 toastMessage,
                 Toast.LENGTH_LONG
         ).show();
+
+        if (answers[mCurrentIndex] == NO_ANSWER){
+            if (currentQuestion.isCorrectAnswer() == answer){
+                numberOfCorrectAnswers++;
+                numberOfAnswers++;
+                answers[mCurrentIndex] = CORRECT_ANSWER;
+            } else {
+                numberOfAnswers++;
+                answers[mCurrentIndex] = INCORRECT_ANSWER;
+            }
+        }
+
+        if (answers[mCurrentIndex] == CORRECT_ANSWER){
+            if (currentQuestion.isCorrectAnswer() != answer){
+                numberOfCorrectAnswers--;
+                answers[mCurrentIndex] = INCORRECT_ANSWER;
+            }
+        }
+
+        if (answers[mCurrentIndex] == INCORRECT_ANSWER){
+            if (currentQuestion.isCorrectAnswer() == answer){
+                numberOfCorrectAnswers++;
+                answers[mCurrentIndex] = CORRECT_ANSWER;
+            }
+        }
     }
 }
